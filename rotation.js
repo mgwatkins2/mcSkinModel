b1ac366a101a5068292f2da8c4be5b5162c8c891
Mae Watkins <mgwatkins2@gmail.com>
2024-08-10 15:50:25 -0400

    Made the rotation much less stiff! Desperately in need of a refactor...

---
+++ rotation.js
@@ -0,0 +1,42 @@
+let constraint = 20;
+let mouseOverContainer = document.getElementById("container");
+let scene = document.getElementById("scene");
+let head = document.getElementById("head");
+
+function bodyTransforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constraint*0.25;
+  let calcY = (x - box.x - (box.width / 2)) / constraint*0.5;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
+
+function headTransforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constraint*1.05;
+  let calcY = (x - box.x - (box.width / 2)) / constraint*1.75;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
+
+ function transformBody(el, xyEl) {
+  el.style.transform  = bodyTransforms.apply(null, xyEl);
+}
+
+function transformHead(el, xyEl) {
+  el.style.transform  = headTransforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([scene]);
+
+  window.requestAnimationFrame(function(){
+    transformBody(scene, position);
+    transformHead(head, position);
+  });
+};

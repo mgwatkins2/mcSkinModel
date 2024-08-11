14d93553c2d477400cb48490eaf2bcd9922b11ba
Mae Watkins <mgwatkins2@gmail.com>
2024-08-10 20:21:04 -0400

    light refactoring.

--- rotation.js
+++ rotation.js
@@ -3,7 +3,8 @@ let mouseOverContainer = document.getElementById("container");
 let model = document.getElementById("model");
 let head = document.getElementById("head");
 
-function bodyTransforms(x, y, el) {
+function bodyTransforms(x, y, el) 
+{
   let box = el.getBoundingClientRect();
   let calcX = -(y - box.y - (box.height / 2)) / constraint*0.25;
   let calcY = (x - box.x - (box.width / 2)) / constraint*0.5;
@@ -13,7 +14,8 @@ function bodyTransforms(x, y, el) {
     + "   rotateY("+ calcY +"deg) ";
 };
 
-function headTransforms(x, y, el) {
+function headTransforms(x, y, el) 
+{
   let box = el.getBoundingClientRect();
   let calcX = -(y - box.y - (box.height / 2)) / constraint*1.05;
   let calcY = (x - box.x - (box.width / 2)) / constraint*1.75;
@@ -23,20 +25,16 @@ function headTransforms(x, y, el) {
     + "   rotateY("+ calcY +"deg) ";
 };
 
- function transformBody(el, xyEl) {
-  el.style.transform  = bodyTransforms.apply(null, xyEl);
-}
-
-function transformHead(el, xyEl) {
-  el.style.transform  = headTransforms.apply(null, xyEl);
-}
-
-mouseOverContainer.onmousemove = function(e) {
+mouseOverContainer.onmousemove = function(e) 
+{
   let xy = [e.clientX, e.clientY];
   let position = xy.concat([model]);
 
-  window.requestAnimationFrame(function(){
-    transformBody(model, position);
-    transformHead(head, position);
+  window.requestAnimationFrame(function()
+  {
+    //transformBody(model, position);
+    model.style.transform  = bodyTransforms.apply(null, position);
+    //transformHead(head, position);
+    head.style.transform  = headTransforms.apply(null, position);
   });
 };
